6f96aa791dceb3d98d9f6175a2c12f34018a9f37
chansuke <moonset20@gmail.com>
2024-03-23 14:59:53 +0900

    Fix function for host api

--- scheduler/plugin/host.go
+++ scheduler/plugin/host.go
@@ -228,7 +228,7 @@ func k8sApiPodInfoFn(ctx context.Context, mod wazeroapi.Module, stack []uint64) 
 	buf := uint32(stack[0])
 	bufLimit := bufLimit(stack[1])
 
-	podInfo := paramsFromContext(ctx).pod
+	podInfo := paramsFromContext(ctx).podInfo
 	stack[0] = uint64(marshalIfUnderLimit(mod.Memory(), podInfo, buf, bufLimit))
 }
 
