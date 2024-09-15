16092ba36958881054d8c87dab6e6a71c6755b10
Kensei Nakada <handbomusic@gmail.com>
2024-09-15 12:55:22 +0900

    address comment

--- guest/api/framework.go
+++ guest/api/framework.go
@@ -3,7 +3,7 @@ package api
 // ImageStateSummary provides summarized information about the state of an image.
 type ImageStateSummary struct {
 	// Size of the image
-	Size int64
+	Size uint64
 	// Used to track how many nodes have this image
-	NumNodes int
+	NumNodes uint32
 }
